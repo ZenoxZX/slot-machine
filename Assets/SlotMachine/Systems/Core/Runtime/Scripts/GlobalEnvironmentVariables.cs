15e465026736bad32965513700531b552b9df434
Aygün ÖZGÜR <aygunozmen@gmail.com>
2026-04-11 02:08:32 +0300

    feat(core): add Core system module

---
+++ Assets/SlotMachine/Systems/Core/Runtime/Scripts/GlobalEnvironmentVariables.cs
@@ -0,0 +1,9 @@
+namespace SlotMachine.Core
+{
+    public static class GlobalEnvironmentVariables
+    {
+        public const string AppName = "SlotMachine";
+        public const string Version = "0.0.1";
+        public const string Author = "Aygün ÖZGÜR";
+    }
+}
