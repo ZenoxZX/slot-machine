3b03ef3d1c1718625dab24dab5db105eca6450aa
Aygün ÖZGÜR <aygunozmen@gmail.com>
2026-04-12 21:05:33 +0300

    feat(slot): add SymbolView, SpinButtonView and mesh effect

---
+++ Assets/SlotMachine/Scripts/Slot/Runtime/View/SpinButtonView.cs
@@ -0,0 +1,36 @@
+using SlotMachine.MessagePipe.Pipes;
+using SlotMachine.Slot.Messages;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace SlotMachine.Slot.View
+{
+    public class SpinButtonView : MonoBehaviour
+    {
+        [SerializeField] private Button m_Button;
+
+        [Inject] private GamePipe m_Pipe;
+
+        private void Start()
+        {
+            m_Button.onClick.AddListener(OnSpinButtonClicked);
+            m_Pipe.SubscribeTo<SpinCompletedMessage>(OnSpinCompleted);
+        }
+
+        private void OnDestroy()
+        {
+            m_Button.onClick.RemoveListener(OnSpinButtonClicked);
+            m_Pipe.UnsubscribeFrom<SpinCompletedMessage>(OnSpinCompleted);
+        }
+
+        private void OnSpinButtonClicked()
+        {
+            m_Button.interactable = false;
+            SpinRequestedMessage message = new();
+            m_Pipe.Raise(in message);
+        }
+
+        private void OnSpinCompleted(ref SpinCompletedMessage _) => m_Button.interactable = true;
+    }
+}
---
+++ Assets/SlotMachine/Scripts/Slot/Runtime/View/SymbolView.cs
@@ -0,0 +1,54 @@
+using LitMotion;
+using SlotMachine.Slot.Core;
+using SlotMachine.Slot.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SlotMachine.Slot.View
+{
+    public class SymbolView : MonoBehaviour
+    {
+        [SerializeField] private Image m_NormalImage;
+        [SerializeField] private Image m_BlurImage;
+
+        private SymbolViewData m_Data;
+        private MotionHandle m_MotionHandle;
+        private bool m_IsBlur;
+        
+        public void SetSymbol(Symbol symbol)
+        {
+            m_NormalImage.sprite = m_Data.GetNormal(symbol);
+            m_BlurImage.sprite = m_Data.GetBlur(symbol);
+        }
+
+        public void SetBlur(bool value)
+        {
+            CancelMotion();
+            m_IsBlur = value;
+
+            m_MotionHandle = LMotion.Create(0f, 1f, m_Data.BlurFadeDuration)
+                .WithEase(Ease.Linear)
+                .Bind
+                (
+                    this,
+                    static (t, self) =>
+                    {
+                        Image normal = self.m_NormalImage;
+                        Image blur = self.m_BlurImage;
+                        float normalAlpha = self.m_IsBlur ? 1f - t : t;
+                        float blurAlpha = self.m_IsBlur ? t : 1f - t;
+                        normal.color = new Color(1f, 1f, 1f, normalAlpha);
+                        blur.color = new Color(1f, 1f, 1f, blurAlpha);
+                    }
+                );
+        }
+
+        private void OnDestroy() => CancelMotion();
+
+        private void CancelMotion()
+        {
+            if (m_MotionHandle.IsActive())
+                m_MotionHandle.Cancel();
+        }
+    }
+}
