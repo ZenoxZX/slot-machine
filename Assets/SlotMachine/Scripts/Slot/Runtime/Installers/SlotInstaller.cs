4efb0c05f8e0684ae2c0ae4d7d50d52cabcc1f06
Aygün ÖZGÜR <aygunozmen@gmail.com>
2026-04-13 14:15:14 +0300

    feat(slot): register view components in installer

--- Assets/SlotMachine/Scripts/Slot/Runtime/Installers/SlotInstaller.cs
+++ Assets/SlotMachine/Scripts/Slot/Runtime/Installers/SlotInstaller.cs
@@ -1,6 +1,8 @@
 using System;
 using SlotMachine.Slot.Controller;
 using SlotMachine.Slot.Data;
+using SlotMachine.Slot.View;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -9,11 +11,22 @@ namespace SlotMachine.Slot.Installers
     [Serializable]
     public class SlotInstaller : IInstaller
     {
+        [SerializeField] private SlotView m_SlotView;
+        [SerializeField] private SpinButtonView m_SpinButtonView;
+        [SerializeField] private SymbolViewData m_SymbolViewData;
+        [SerializeField] private ReelContainer m_ReelContainer;
+
         public void Install(IContainerBuilder builder)
         {
             builder.RegisterEntryPoint<SpinController>().AsSelf();
             builder.RegisterEntryPoint<SpinResultProvider>();
             builder.RegisterEntryPoint<SpinResultPersistence>();
+
+            builder.RegisterComponent<ISlotView>(m_SlotView);
+            builder.RegisterComponent<SpinButtonView>(m_SpinButtonView);
+
+            builder.RegisterInstance(m_SymbolViewData).AsSelf();
+            builder.RegisterInstance(m_ReelContainer).AsSelf();
         }
     }
 }
